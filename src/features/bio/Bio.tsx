77a0605fa08afdbe75046dc879c627169d781270
Mónica Bernal <mok2710@hotmail.com>
2023-04-04 21:34:36 -0500

    created button active and button inactive component

--- src/features/bio/Bio.tsx
+++ src/features/bio/Bio.tsx
@@ -1,29 +1,27 @@
 import { useState } from "react";
 import { NombresSimpsons, INFO_SIMPSONS } from "./constants";
-import styles from "./styles.module.css";
-import { BioContainer, ContenedorBotones, BioImagen,} from "./styled";
+import { BioContainer, ContenedorBotones, BioImagen, BioNombre, BioDescripcion, ButtonActive, ButtonInactive } from "./styled";
 
 const Bio = () => {
   const [bioActiva, setBioActiva] = useState(
     INFO_SIMPSONS[NombresSimpsons.BART]
   );
 
-  const onClick: (nombre: NombresSimpsons) => void = (nombre) =>
+  const test: (nombre: NombresSimpsons) => void = (nombre) =>
     setBioActiva(INFO_SIMPSONS[nombre]);
 
   const crearBotones = () => {
     return Object.keys(INFO_SIMPSONS).map((nombre: string) => (
-      <button
-        key={nombre as string}
-        onClick={() => onClick(nombre as NombresSimpsons)}
-        className={
-          bioActiva.id === nombre
-            ? styles.botonBioActivo
-            : styles.botonBioInactivo
-        }
-      >
-        {nombre}
-      </button>
+      bioActiva.id === nombre ?
+        <ButtonActive
+          key={nombre as string}
+          onClick={() => test(nombre as NombresSimpsons)}
+        >
+          {nombre}
+        </ButtonActive>
+
+        : <ButtonInactive key={nombre as string} onClick={() => test(nombre as NombresSimpsons)}> {nombre}</ButtonInactive>
+
     ));
   };
 
@@ -38,8 +36,8 @@ const Bio = () => {
           />
         </div>
         <div>
-          <h3 className={styles.bioNombre}>{bioActiva.nombre}</h3>
-          <p className={styles.bioDescripcion}>{bioActiva.descripcion}</p>
+          <BioNombre>{bioActiva.nombre}</BioNombre>
+          <BioDescripcion>{bioActiva.descripcion}</BioDescripcion>
         </div>
       </div>
     </BioContainer>
